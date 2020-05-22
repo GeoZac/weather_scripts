957cbf343395487ae174eae64c737d4602df3d42
George Zacharia <george.zcharia@gmail.com>
2020-05-22 11:21:01 +0530

    Get active cyclones

--- hurricane.py
+++ hurricane.py
@@ -5,15 +5,16 @@ from haversine import haversine
 
 url = "https://api.aerisapi.com/tropicalcyclones/?&filter=all&limit=5&format=json&{}".format(api_key)
 
-# home = home.split(",")
-# loc_home = float(home[0]), float(home[1])
-# 
-# def fetch_data():  # Get the current data from weather channel
-#     resp = get(url)
-#     data = resp.json()
-#     return data
-#
-#
+home = home.split(",")
+loc_home = float(home[0]), float(home[1])
+
+
+def fetch_data():  # Get the current data from weather channel
+    resp = get(url)
+    data = resp.json()
+    return data
+
+
 # def fetch_hurricane(name):  # Fetch only the one passed as query
 #     data = fetch_data()
 #     hurricanes = data['currenthurricane']
@@ -52,35 +53,23 @@ url = "https://api.aerisapi.com/tropicalcyclones/?&filter=all&limit=5&format=jso
 #         print("{} is not a currently active Cyclone".format(query))
 #         print("Currently active ones are:")
 #         get_current()
-#
-#
-# def get_current():  # Fetches current Tropical Cyclones,to get the name to be passed in case of get_data()
-#     data = fetch_data()
-#     ch_list = []
-#     try:
-#         hurricanes = data['currenthurricane']
-#     except KeyError:
-#         hurricanes = []
-#         exit("No Active cyclones tracked")
-#     i = 0
-#     for i in range(0, len(hurricanes)):
-#         name = hurricanes[i]['stormInfo']['stormName']
-#         loc = hurricanes[i]['Current']['lat'], hurricanes[i]['Current']['lon']
-#         how_far = round(haversine(loc, loc_home), ndigits=2)
-#         ch_list.append(name)
-#         print(i + 1, name, how_far, "km")
-#     ch = int(input("Enter no of hurricane to track: "))
-#     if ch < i + 2:
-#         get_data(str(ch_list[ch - 1]))
-#     else:
-#         exit(0)
-#
-#
-# # def pprint(name, loc, how_far, wind_speed, movement):
-#
-#
-# if __name__ == '__main__':
-#     try:
-#         get_data(sys.argv[1])
-#     except IndexError:
-#         get_current()
+
+
+def get_current():  # Fetches current Tropical Cyclones,to get the name to be passed in case of get_data()
+    data = fetch_data()
+    current = data["response"]
+    index = 1
+    if not current:
+        print("No Active cyclones tracked")
+    else:
+        for item in current:
+            name = item["profile"]["name"]
+            coor = item["position"]["location"]["coordinates"]
+            how_far = round(haversine(reversed(coor), loc_home), ndigits=2)
+            print(index, name, how_far, "km")
+        selection = int(input("Enter no of hurricane to track: "))
+        # get_data(current, selection - 1)
+
+
+if __name__ == "__main__":
+    get_current()
