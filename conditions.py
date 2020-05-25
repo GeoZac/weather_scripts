8a005f56363423cccb6dfc0ebfa366607f79f44e
George Zacharia <george.zcharia@gmail.com>
2020-05-25 10:20:36 +0530

    Start migrating conditions to AeriesWeather API
    This seems like a mess, but I have to start somewhere

--- conditions.py
+++ conditions.py
@@ -1,24 +1,24 @@
 import requests
 import sys
-from config import api_key
+from config import api_key, home
 
 
 # noinspection PyUnusedLocal
 def get_weather(loc):
-    lookup_url = "http://api.wunderground.com/api/{}/geolookup/q/India/".format(api_key)
-    condition_url = "http://api.wunderground.com/api/{}/conditions/q/India/".format(api_key)
+    lookup_url = "https://api.aerisapi.com/places/search?query=name:{}&limit=10&{}".format(loc, api_key)
+    condition_url = "https://api.aerisapi.com/observations/{}?&format=json&filter=allstations&limit=1&{}".format(home, api_key)
 
     degree_sign = u'\N{DEGREE SIGN}'
-    geolookup = requests.get(lookup_url + loc + ".json")
+    geolookup = requests.get(lookup_url)
     # if (len(geolookup.json())) == 2:
     try:
-        resp = requests.get(condition_url + loc + ".json")
-        condition = resp.json()['current_observation']['weather']
+        resp = requests.get(condition_url)
+        condition = resp.json()["response"]["ob"]["weatherShort"]
         # temp_c = str(resp.json()['current_observation']['temp_c']) + '' + degree_sign + "C"
         # temp_f = str(resp.json()['current_observation']['temp_f']) + '' + degree_sign + "F"
         # temp = temp_c + "/" +temp_f
-        temp = resp.json()['current_observation']['temperature_string']
-        full_location = resp.json()['current_observation']['display_location']['full']
+        temp = resp.json()["response"]["ob"]["tempC"]
+        full_location = resp.json()["response"]["place"]["name"]
         weather = "The current temperature in {0} is {1}.".format(
             full_location, temp)
         print(weather)
